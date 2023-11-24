c5a27a42ef666c6d9c05824d4005eb828e5243a8
RRR8RR <139396961+RRR8RR@users.noreply.github.com>
2023-11-24 15:42:26 +0300

    Update تحديث.py

--- SedUb/plugins/تحديث.py
+++ SedUb/plugins/تحديث.py
@@ -36,7 +36,7 @@ heroku_api = "https://api.heroku.com"
 UPSTREAM_REPO_BRANCH = Config.UPSTREAM_REPO_BRANCH
 
 REPO_REMOTE_NAME = "temponame"
-IFFUCI_ACTIVE_BRANCH_NAME = "SedUb"
+IFFUCI_ACTIVE_BRANCH_NAME = "main"
 NO_HEROKU_APP_CFGD = "no heroku application found, but a key given? 😕 "
 HEROKU_GIT_REF_SPEC = "HEAD:refs/heads/main"
 RESTARTING_APP = "re-starting heroku application"
