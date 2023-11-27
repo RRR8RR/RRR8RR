ccfdff498090d76bb79fa778091e339e51f8dbd7
RRR8RR <139396961+RRR8RR@users.noreply.github.com>
2023-11-27 16:30:28 +0300

    Update الانتحال.py

--- SedUb/plugins/الانتحال.py
+++ SedUb/plugins/الانتحال.py
@@ -110,7 +110,7 @@ async def _(event):
         await event.client.send_message(
             BOTLOG_CHATID, f"⌁︙تـم اعادة الـحساب الى وضـعه الاصلـي ،✅")
 #Reda
-jeps = ["xLxLxLrr3", "tipthon_help"]
+jeps = ["xLxLxLrr3", "xLxLxLrr7"]
 @l313l.ar_cmd(pattern="انتحال_الدردشه")
 async def reda(event):
     if event.is_group or event.is_channel:
